32961ba11dfcbb9d882f4ca6bc443f1f75e70978
Carmen Chow <carmendevelops@gmail.com>
2022-12-20 14:48:54 -0500

    created list and keys

--- src/App.js
+++ src/App.js
@@ -2,22 +2,66 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const books = [
+    {
+      id: 1,
+      title: 'Afterlives',
+      author: 'Abdulrazak Gurnah',
+      checked: false
+    },
+    {
+      id: 2,
+      title: 'Stay True',
+      author: 'Hua Hsu',
+      checked: false
+    },
+    {
+      id: 3,
+      title: 'Young Mungo',
+      author: 'Douglas Stuart',
+      checked: false
+    },
+    {
+      id: 4,
+      title: 'Mecca',
+      author: 'Susan Straight',      
+      checked: false
+    },
+    {
+      id: 5,
+      title: 'Butts: A Backstory',
+      author: 'Heather Radke',
+      checked: false
+    },
+    {
+      id: 6,
+      title: 'Tomorrow and Tomorrow and Tomorrow',
+      author: 'Gabrielle Zevin',
+      checked: false
+    },
+    {
+      id: 7,
+      title: 'Young Mungo',
+      author: 'Douglas Stuart',
+      checked: false
+    },
+    {
+      id: 8,
+      title: 'Losing the Plot',
+      author: 'Derek Owusu',
+      checked: false
+    },
+  ]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The 10 Must-Read Books of 2022</h1>
+        {books.map((book) => (
+          <li className="book" key={book.id}>
+            <input type="checkbox" checked={book.checked}/>
+          <p>{book.title} by {book.author}</p>      
+          </li>
+        ))}
     </div>
   );
 }
